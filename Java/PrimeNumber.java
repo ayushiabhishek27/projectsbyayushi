f987ffcc28a1245ad8fbfc37e3c70d46b86deecf
ayushiabhishek27 <ayushi270105@gmail.com>
2026-02-14 01:16:29 +0530

    Add PrimeCheck class to determine prime numbers

---
+++ Java/PrimeNumber.java
@@ -0,0 +1,17 @@
+public class PrimeCheck {
+    public static void main(String[] args) {
+        int n = 29;
+        boolean isPrime = true;
+
+        if(n <= 1) isPrime = false;
+
+        for(int i = 2; i <= Math.sqrt(n); i++) {
+            if(n % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        System.out.println(isPrime ? "Prime" : "Not Prime");
+    }
+}
