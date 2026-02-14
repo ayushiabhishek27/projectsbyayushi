1c8acc9998a8911d54638a9f18fc9e3ad93aa229
Ayushi Abhishek <ayushi270105@gmail.com>
2026-02-14 23:44:04 +0530

    Implement Fibonacci series generator with user input
    Added a method to generate Fibonacci series and user input for terms.

--- Java/FibonacciSeries.java
+++ Java/FibonacciSeries.java
@@ -1,3 +1,25 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+
+    public static void fibonacci(int n){
+        int a=0,b=1;
+        for(int i=1;i<=n;i++){
+            System.out.print(a+" ");
+            int next=a+b;
+            a=b;
+            b=next;
+        }
+    }
+
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter terms: ");
+        int n=sc.nextInt();
+
+        fibonacci(n);
+    }
+}
 public class Fibonacci {
     public static void main(String[] args) {
         int n = 10, a = 0, b = 1;
