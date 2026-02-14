499bdcc009180227e7e37869f6951e18eccac277
Ayushi Abhishek <ayushi270105@gmail.com>
2026-02-14 23:43:27 +0530

    Refactor to use largest method for finding max
    Refactored to separate largest element logic into a method.

--- Java/Largest Element.java
+++ Java/Largest Element.java
@@ -1,13 +1,24 @@
+import java.util.Scanner;
+
 public class LargestElement {
-    public static void main(String[] args) {
-        int[] arr = {5, 9, 2, 14, 7};
-        int max = arr[0];
 
-        for(int i = 1; i < arr.length; i++) {
-            if(arr[i] > max)
-                max = arr[i];
-        }
+    public static int largest(int[] arr){
+        int max=arr[0];
+        for(int num:arr)
+            if(num>max) max=num;
+        return max;
+    }
 
-        System.out.println("Largest: " + max);
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter size: ");
+        int n=sc.nextInt();
+
+        int[] arr=new int[n];
+        System.out.println("Enter elements:");
+        for(int i=0;i<n;i++)
+            arr[i]=sc.nextInt();
+
+        System.out.println("Largest: "+largest(arr));
     }
 }
