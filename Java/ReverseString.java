b36a9cad76a7e0dbd40529b165529e3a9b4782f1
Ayushi Abhishek <ayushi270105@gmail.com>
2026-02-14 23:42:04 +0530

    Refactor ReverseString to use a separate reverse method

--- Java/ReverseString.java
+++ Java/ReverseString.java
@@ -1,12 +1,19 @@
+import java.util.Scanner;
+
 public class ReverseString {
-    public static void main(String[] args) {
-        String str = "Mayank";
-        String rev = "";
 
-        for(int i = str.length() - 1; i >= 0; i--) {
+    public static String reverse(String str){
+        String rev="";
+        for(int i=str.length()-1;i>=0;i--)
             rev += str.charAt(i);
-        }
+        return rev;
+    }
 
-        System.out.println("Reversed: " + rev);
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter string: ");
+        String input=sc.nextLine();
+
+        System.out.println("Reversed: "+reverse(input));
     }
 }
